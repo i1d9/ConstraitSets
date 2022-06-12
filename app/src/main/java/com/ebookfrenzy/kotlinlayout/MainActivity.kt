0369f7eccfaa59718c78a92c0558f48aea23244d
i1d9 <42600121+i1d9@users.noreply.github.com>
2022-06-12 23:59:27 +0300

    UI

--- app/src/main/java/com/ebookfrenzy/kotlinlayout/MainActivity.kt
+++ app/src/main/java/com/ebookfrenzy/kotlinlayout/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import androidx.constraintlayout.widget.ConstraintLayout
 import android.widget.Button
 import android.widget.EditText
+import androidx.constraintlayout.widget.ConstraintSet
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +19,24 @@ class MainActivity : AppCompatActivity() {
         val myButton = Button(this)
         myButton.text = getString(R.string.press_me)
         myButton.setBackgroundColor(Color.YELLOW)
+        myButton.id = R.id.myButton
 
         val myLayout = ConstraintLayout(this)
         myLayout.setBackgroundColor(Color.BLUE)
 
         myLayout.addView(myButton)
         setContentView(myLayout)
+
+        val set = ConstraintSet()
+
+        set.constrainHeight(myButton.id, ConstraintSet.WRAP_CONTENT)
+        set.constrainWidth(myButton.id, ConstraintSet.WRAP_CONTENT)
+
+        set.connect(myButton.id, ConstraintSet.START, ConstraintSet.PARENT_ID,ConstraintSet.START, 0)
+        set.connect(myButton.id, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END, 0)
+        set.connect(myButton.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID,ConstraintSet.TOP, 0)
+        set.connect(myButton.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, 0)
+
+        set.applyTo(myLayout)
     }
 }
