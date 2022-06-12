17840296999a37200ab934eed14d0cfb43929f6b
i1d9 <42600121+i1d9@users.noreply.github.com>
2022-06-13 00:25:10 +0300

    Px to Dp function

--- app/src/main/java/com/ebookfrenzy/kotlinlayout/MainActivity.kt
+++ app/src/main/java/com/ebookfrenzy/kotlinlayout/MainActivity.kt
@@ -8,11 +8,19 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import android.widget.Button
 import android.widget.EditText
 import androidx.constraintlayout.widget.ConstraintSet
+import android.content.res.Resources
+import android.util.TypedValue
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         configureLayout()
+    }
+
+    private fun convertToPx(value: Int) : Int{
+        val r = resources
+        val px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value.toFloat(), r.displayMetrics).toInt()
+        return px
     }
 
     private fun configureLayout() {
@@ -21,10 +29,17 @@ class MainActivity : AppCompatActivity() {
         myButton.setBackgroundColor(Color.YELLOW)
         myButton.id = R.id.myButton
 
+        val myEditText = EditText(this)
+        myEditText.id = R.id.myEditText
+
+        myEditText.width = convertToPx(200)
+
         val myLayout = ConstraintLayout(this)
         myLayout.setBackgroundColor(Color.BLUE)
 
         myLayout.addView(myButton)
+        myLayout.addView(myEditText)
+
         setContentView(myLayout)
 
         val set = ConstraintSet()
@@ -36,6 +51,14 @@ class MainActivity : AppCompatActivity() {
         set.connect(myButton.id, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END, 0)
         set.connect(myButton.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID,ConstraintSet.TOP, 0)
         set.connect(myButton.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, 0)
+
+        set.constrainHeight(myEditText.id, ConstraintSet.WRAP_CONTENT)
+        set.constrainWidth(myEditText.id, ConstraintSet.WRAP_CONTENT)
+
+
+        set.connect(myEditText.id, ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT,0)
+        set.connect(myEditText.id, ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT,0)
+        set.connect(myEditText.id, ConstraintSet.BOTTOM, myButton.getId(), ConstraintSet.TOP,70)
 
         set.applyTo(myLayout)
     }
