2eff41d4a4c241d1fc7a7616ff9e6ecb452ea637
i1d9 <42600121+i1d9@users.noreply.github.com>
2022-06-12 23:49:23 +0300

    Background Color Changed

--- app/src/main/java/com/ebookfrenzy/kotlinlayout/MainActivity.kt
+++ app/src/main/java/com/ebookfrenzy/kotlinlayout/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.ebookfrenzy.kotlinlayout
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import androidx.constraintlayout.widget.ConstraintLayout
+import android.widget.Button
+import android.widget.EditText
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        configureLayout()
+    }
+
+    private fun configureLayout() {
+        val myButton = Button(this)
+        myButton.text = getString(R.string.press_me)
+        myButton.setBackgroundColor(Color.YELLOW)
+
+        val myLayout = ConstraintLayout(this)
+        myLayout.setBackgroundColor(Color.BLUE)
+
+        myLayout.addView(myButton)
+        setContentView(myLayout)
     }
 }
